8b9a1b54b422bf8b7f1067e0aff1df3686934584
Edoardo Colombo <edo.gcolombo@gmail.com>
2015-10-21 17:23:29 +0200

    Removed useless constants.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,3 @@
-
-export const Directions = {
-    DECK_TO_PILE: 'DECK_TO_PILE',
-    DECK_TO_FOUNDATION: 'DECK_TO_FOUNDATION',
-    PILE_TO_FOUNDATION: 'PILE_TO_FOUNDATION',
-    FOUNDATION_TO_PILE: 'FOUNDATION_TO_PILE'
-}
-
 export function moveCard(cards, where) {
     return {
         type: 'MOVE_CARD',
