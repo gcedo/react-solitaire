2c1d1784635fe2caa1866365118f1319a2670dc9
Edoardo Colombo <edo.gcolombo@gmail.com>
2015-10-30 18:13:08 +0100

    Added build job.

---
+++ src/index.prod.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Game from './components/controller/Game.jsx';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+const store = createStore(reducers);
+ReactDOM.render(
+    <Provider store={store}>
+        <Game />
+    </Provider>,
+    document.getElementById('game')
+);
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'source-map',
   entry: [
-    './src/index'
+    './src/index.prod'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
@@ -26,7 +26,7 @@ module.exports = {
   ],
   module: {
     loaders: [{
-      test: /\.js$/,
+      test: /\.jsx?$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
     }]
