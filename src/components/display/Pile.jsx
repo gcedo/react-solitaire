f1aad1f55eec05788e3b3f758299087f77e909d3
vzvu3k6k <vzvu3k6k@gmail.com>
2018-02-05 00:02:47 +0900

    Update react and its related packages
    As PropTypes has been extracted into prop-types package, add
    prop-types to dependencies and update import statements.

--- src/components/display/Pile.jsx
+++ src/components/display/Pile.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes as T } from 'react';
+import React from 'react';
+import T from 'prop-types';
 import RankSymbol from './RankSymbol.jsx';
 import { Colors, Dimensions, CardsLayouts } from '../../constants';
 
